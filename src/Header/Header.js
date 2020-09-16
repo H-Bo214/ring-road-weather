21b107f954807a39a1b3165458224c95240de188
Horacio Borrego <hborrego365@gmail.com>
2020-09-16 17:19:43 -0600

    Add general favorite button styling,  Add Header structure to Header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../Header/Header.css';
 import route1 from '../assets/route1.png'
-
+import { Link } from 'react-router-dom'
 function Header(props) {
   return(
     <header>
-      <img 
-        className="route1-img"
-        src={route1} 
-        alt="road sign for Iceland's Route 1"
-      />
+      <Link to="/">
+        <img 
+          className="route1-img"
+          src={route1} 
+          alt="road sign for Iceland's Route 1"
+        />
+      </Link>
       <h1>Ring Road Weather!</h1>
+      <Link to="/favorites">
+        <button className="header-fav-btn" type="button">Favorites</button>
+      </Link>
     </header>
   )
 }
