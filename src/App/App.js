86fa2cad77e3d07a6e340734e604618ebad65c55
Horacio Borrego <hborrego365@gmail.com>
2020-09-17 08:59:08 -0600

    Add favorites and error to App state

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import '../App/App.css';
 
-// Add api import once method set up
+import { fetchWeather } from '../apiCalls'
 import Header from '../Header/Header'
 import DetailsPage from '../DetailsPage/DetailsPage'
 import FavoritesPage from '../FavoritesPage/FavoritesPage'
@@ -15,21 +15,39 @@ class App extends Component {
     this.state = {
       currentWeather: {},
       fiveDayForecast: [],
-      favorites: []
+      favorites: [],
+      error: '',
     };
-
+    this.fetchWeather = fetchWeather
   }
 
   componentDidMount() {
 
   }
 
+  handleFetch = async (cityName) => {
+   try {
+    const data = await this.fetchWeather(cityName)
+    console.log('data', data)
+   } catch (error) {
+     this.setState({error: 'An error occurred fetching weather.'})
+   }
+
+  }
+
   render() {
     return(
+      <Router>
       <main className="App">
         <Header />
-        <Form  />
+        <Form
+          handleFetch={this.handleFetch}
+        
+        
+        />
+        {/* <DetailsPage /> */}
       </main>
+      </Router>
     )
   }
 
