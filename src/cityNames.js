fedd5bb2687a7cba4ed49f1b6c62c948966156cb
Horacio Borrego <hborrego365@gmail.com>
2020-09-16 18:54:51 -0600

    Create dataSet for Icelandic cities

---
+++ src/cityNames.js
@@ -0,0 +1,30 @@
+export const southIceland = [
+  "Reykjavik",
+  "Hveragerdi",
+  "Arborg",
+  "Hella",
+  "Hvolsvollur",
+  "Vik",
+];
+
+export const eastIceland = [
+  "Bjarnanes",
+  "Hofn",
+  "Fáskrúðsfjörður",
+  "Reydarfjordur",
+  "Egilsstaðir",
+  "Fellabær",
+];
+
+export const northIceland = [
+  "Reykjahlíð", 
+  "Akureyri", 
+  "Möðruvellir"
+];
+
+export const westIceland = [
+  "Borgarnes",
+  "Hofn",
+  "Grundarhverfi",
+  "Mosfellsbær",
+];
