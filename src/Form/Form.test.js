b87da92ce485f938a462ff3e0cbfac7fa28604c0
Horacio Borrego <hborrego365@gmail.com>
2020-09-19 21:10:03 -0600

    Add additiona form tests

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Form from '../Form/Form'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('Form', () => {
-  let city, handleFetch
+  let handleFetch
   beforeEach( () => {
 
-    city = 'Akureyri'
+
     handleFetch = jest.fn()
     render(
     <MemoryRouter>
@@ -29,13 +29,33 @@ describe('Form', () => {
     expect(regionTitle).toBeInTheDocument()
   })
 
-  it('should have a selected option value', () => {
+  it('should have a selected option value for a region', () => {
     const region = screen.getByRole('option', {name: 'North Iceland'})
     expect(region).toBeInTheDocument()
   })
 
+  it('should have a selected option value for a city', () => {
+    const pickCity= screen.getByText('Pick a city')
+    expect(pickCity).toBeInTheDocument()
+  })
+
+  it('should be able to select a region', () => {
+    const selectRegion = screen.getByRole('option', {name: 'Pick a region'})
+    expect(selectRegion).toBeInTheDocument()
+
+    fireEvent.change(selectRegion, {target:{value:'South Iceland'}})
+    
+    const southIceland = screen.getByRole('option', {name: 'South Iceland'})
+    expect(southIceland).toBeInTheDocument()
+    expect(selectRegion.value).toBe('South Iceland')
+  })
 
 
+
+  // it('should have a get weather button', () => {
+  //   const button = screen.getByRole('button')
+  //   expect(button).toBeInTheDocument()
+  // })
 
 
 
