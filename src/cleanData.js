3ae5adef95a309656354add2e2531e56474e4c15
Horacio Borrego <hborrego365@gmail.com>
2020-12-01 10:49:17 -0700

    Format day to display in a readable form

--- src/cleanData.js
+++ src/cleanData.js
@@ -1,11 +1,18 @@
+import { DateTime } from 'luxon';
+
 const capitalize = (words) => {
   return words.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 };
 
 export const cleanData = (data) => {
+  let m = DateTime.fromSeconds(data.dt).monthLong
+  let d = DateTime.fromSeconds(data.dt).day
+  let dayOfWeek = DateTime.fromSeconds(data.dt).weekdayShort
+  let dateFormat = `${dayOfWeek}, ${m} ${d} `
+
   const weatherData = {
     cloudCover: data.clouds.all,
-    time: data.dt ,
+    time: dateFormat ,
     feelsLike: data.main.feels_like,
     humidity: data.main.humidity,
     temperature: data.main.temp,
@@ -18,5 +25,8 @@ export const cleanData = (data) => {
     windSpeed: data.wind.speed,
     id:data.id,
   }
+  console.log('timeData', DateTime.fromSeconds(data.dt))
+  // console.log('Formatting', console.log({m:m, d:d, dayOfWeek:dayOfWeek}))
+  console.log('dateFormat', dateFormat)
   return weatherData
 }
